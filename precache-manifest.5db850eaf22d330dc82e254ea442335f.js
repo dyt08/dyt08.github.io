93f173848691baf5f277ec0d90b177030040519b
Aditya Nugraha <15890809+dyt08@users.noreply.github.com>
2021-03-13 23:31:25 +0700

    Updates

---
+++ precache-manifest.5db850eaf22d330dc82e254ea442335f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8efcb72c2c26631127829a36e6d04deb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae4ec3deb08546b026ff",
+    "url": "/static/css/main.fedf132f.chunk.css"
+  },
+  {
+    "revision": "4b48c8c310ad002ec737",
+    "url": "/static/js/2.38ac9ecf.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.38ac9ecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae4ec3deb08546b026ff",
+    "url": "/static/js/main.942a8190.chunk.js"
+  },
+  {
+    "revision": "91a46a0099d0057487f0",
+    "url": "/static/js/runtime-main.7870e996.js"
+  },
+  {
+    "revision": "37b304657d25bad933d77f335e11af7c",
+    "url": "/static/media/photo.37b30465.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.403a73fc8e4fe09219eb5afc130ac5f8.js"
+  "/precache-manifest.5db850eaf22d330dc82e254ea442335f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
